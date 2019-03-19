e2a9fb8ba3797037fe4ed1247ba122e7320e509f
Peter Wildberger <pete.wildberger@gmail.com>
2019-03-18 21:22:11 -0500

    ready to add listeners dynamically

--- app/src/main/java/com/example/PianoApp/MainActivity.kt
+++ app/src/main/java/com/example/PianoApp/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.PianoApp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.initKeys()
+    }
+    private fun initKeys(){
+        val r = resources
+        val name = packageName
+        var ref = findViewById<Button>(r.getIdentifier("b3", "id", name))
+        ref.setOnClickListener {
+
+        }
+//        example
+//        val cells = IntArray(81)
+//        for (i in 0..80) {
+//            if (i < 10)
+//                cells[i] = r.getIdentifier("Squares0$i", "id", name)
+//            else
+//                cells[i] = r.getIdentifier("Squares$i", "id", name)
+//        }
     }
 }
