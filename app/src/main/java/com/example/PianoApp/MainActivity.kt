d662018b85554c13f9b6e80da75e700739ceec8d
Peter Wildberger <pete.wildberger@gmail.com>
2019-03-19 10:36:44 -0500

    converted Java code to Kotlin for generating tones now playing on click

--- app/src/main/java/com/example/PianoApp/MainActivity.kt
+++ app/src/main/java/com/example/PianoApp/MainActivity.kt
@@ -1,30 +1,112 @@
 package com.example.PianoApp
 
+import android.annotation.SuppressLint
+import android.media.AudioFormat
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.media.AudioTrack
+import android.media.AudioFormat.ENCODING_PCM_16BIT
+import android.media.AudioFormat.CHANNEL_OUT_MONO
+import android.media.AudioManager
+import android.media.AudioFormat.ENCODING_PCM_8BIT
+import android.os.Build
+import android.media.AudioAttributes
+import android.R.layout.*
+import android.os.Handler
+import kotlin.experimental.and
+
+private val TAG = "MyActivity"
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private val duration = 5 // seconds
+    private val sampleRate = 44100
+    private val numSamples = duration * sampleRate
+    private val sample = DoubleArray(numSamples)
+    private val freqOfTone = 440.0 // hz
+
+
+
+    private var handler = Handler()
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        this.initKeys()
-    }
-    private fun initKeys(){
+//        val thread = Thread(Runnable {
+//            genTone()
+//            handler.post(Runnable { playSound() })
+//        })
+        val tone: ByteArray = genTone(freqOfTone)
+
         val r = resources
         val name = packageName
         var ref = findViewById<Button>(r.getIdentifier("b3", "id", name))
         ref.setOnClickListener {
-
+            ref.isSoundEffectsEnabled = false
+//            thread.start()
+            playSound(tone)
         }
-//        example
-//        val cells = IntArray(81)
-//        for (i in 0..80) {
-//            if (i < 10)
-//                cells[i] = r.getIdentifier("Squares0$i", "id", name)
-//            else
-//                cells[i] = r.getIdentifier("Squares$i", "id", name)
-//        }
     }
+
+    private fun genTone(freq: Double): ByteArray {
+        val generatedSnd = ByteArray(2 * numSamples)
+        // fill out the array
+        for (i in 0 until numSamples) {
+            // sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
+            sample[i] = Math.sin((2 * Math.PI - .001) * i / (sampleRate / freq))
+        }
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        var idx = 0
+        for (dVal in sample) {
+            val value = (dVal * 32767).toShort()
+            val sixteen: Short = (0xff00).toShort()
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (value and 0x00ff).toByte()
+            generatedSnd[idx++] = (value and sixteen).toInt().ushr(8).toByte()
+        }
+        return generatedSnd
+    }
+
+    @SuppressLint("NewApi")
+    private fun playSound(generatedSnd: ByteArray) {
+        var mAudioTrack: AudioTrack
+        // AudioTrack definition
+        val mBufferSize = AudioTrack.getMinBufferSize(
+            44100,
+            AudioFormat.CHANNEL_OUT_STEREO,
+            AudioFormat.ENCODING_PCM_16BIT
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mAudioTrack = AudioTrack.Builder()
+                .setAudioAttributes(
+                    AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .build()
+                )
+                .setAudioFormat(
+                    AudioFormat.Builder()
+                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                        .setSampleRate(44100)
+                        .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+                        .build()
+                )
+                .setBufferSizeInBytes(mBufferSize)
+                .build()
+            mAudioTrack.setVolume(1.0F)
+        }else {
+            mAudioTrack = AudioTrack(
+                AudioManager.STREAM_MUSIC, 44100,
+                AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT,
+                mBufferSize, AudioTrack.MODE_STREAM
+            )
+        }
+        mAudioTrack.write(generatedSnd, 0, numSamples)
+//        mAudioTrack.setLoopPoints(1,mBufferSize-1, -1)
+        mAudioTrack.play()
+    }
+
 }
